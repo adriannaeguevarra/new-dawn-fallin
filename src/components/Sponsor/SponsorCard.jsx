5ee935bfbeeea374bd87d967e04ec3f342858f96
timothylewis91 <timothylewis91@yahoo.com>
2022-11-19 16:33:14 -0800

    Update Space Backend

---
+++ src/components/Sponsor/SponsorCard.jsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+const SponsorCard = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://bit.ly/3UPkw6Q"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          FName LName
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          test123@gmail.com
+        </Typography>
+
+        <Typography variant="body2" color="text.primary" container spacing={2}>
+          Sponsoring #
+        </Typography>
+        <Typography
+          align="right"
+          variant="body3"
+          sx={{ color: `secondary.main` }}
+        >
+          Cases Completed
+        </Typography>
+      </CardContent>
+      <div>
+        <Button variant="outlined" onClick={handleClickOpen}>
+          Information
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Background"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Providing necessities for homeless and disabled and at-risk
+              veterans. Comfort Kits are distributed primarily through Veteran
+              Stand Downs nationally. Comfort Kits can include items such as
+              blankets, toiletries and clothing.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Disagree</Button>
+            <Button onClick={handleClose} autoFocus>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    </Card>
+  );
+};
+export default SponsorCard;
---
+++ src/components/Veteran/VeteranCard.jsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+const VetCards = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ borderRadius: "50%" }}
+        component="img"
+        image="https://bit.ly/3AvqTEd"
+        alt="Veteran Profile"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          First Last M.
+        </Typography>
+        <Typography variant="body1" color="text.primary">
+          Sponsor M. LastName
+        </Typography>
+        <Typography variant="body1" color="text.secondary">
+          Military Branch
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          1775-2010
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <div>
+          <Button variant="outlined" onClick={handleClickOpen}>
+            Edit Profile
+          </Button>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Edit Profile</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To edit Profile and saves changes to their information. The form
+                will pop out as a Modal for them to make any changes
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Form will be imported here"
+                type="email"
+                fullWidth
+                variant="standard"
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleClose}>Subscribe</Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default VetCards;
