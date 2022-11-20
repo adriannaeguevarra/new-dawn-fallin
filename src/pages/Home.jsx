fd1d201d86af93b1f6d33393d7fb62a90ba71f23
Adrianna Guevarra <adrianna.guevarra@sdvi.com>
2022-11-20 03:27:18 -0800

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,132 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { Auth } from 'aws-amplify';
-import ButtonAppBar from '../components/navbar'
-import { Login } from '../components/Login';
-import { Button, Card, CardActions, CardContent, CardMedia, Grid, List, ListItem, ListItemText, Typography } from '@mui/material';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import logo from '../assets/logo.png'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import Logo from '../assets/logo.png'
+import { useNavigate } from 'react-router-dom'
+
 
 function Home() {
-    const Navigate = useNavigate();
-    const [isSignedIn, setIsSignedIn] = useState(false);
-    
-    
-    useEffect(() => {
-       
-        Auth.currentAuthenticatedUser()
-            .then(res => {
-            setIsSignedIn(res);
-            })
-            .catch(err => {
-                console.warn(err);
-            })
-    },[])
-    
-    console.log(isSignedIn)
-   const beSponsor = () => {
-        Navigate("/login")
-    }
-    if (isSignedIn) {
-        Navigate("/profile")
-    }
-    return (
-        <>
-            <Grid container component="main" spacing={2} sx={{ height: '70vh', width: '60vw' }}>
-                <Grid xs={12}>
-                    <ButtonAppBar />
-                </Grid>
-                <Grid item sm={12} md={8}
-                >
-                    <Card >
-                        <CardMedia
-                            component="img"
-                            alt="green iguana"
-                            height="140"
-                            image="https://source.unsplash.com/random/?veteran"
-                            sx={{ padding: "2em 2em 2em 2em", objectFit: "fit" , height:'80vh'}}
-                        >
-                        </CardMedia>
-                    </Card>
-                </Grid>
-                <Grid item sm={12} md={4}>
-                    <Card sx={{ minWidth: 275, backgroundColor: "#575741" }}>
-                        <CardMedia
-                            component="img"
-                            alt="green iguana"
-                            height="140"
-                            image={logo}
-                            sx={{ padding: "2em 2em 2em 2em", objectFit: "contain" }}
-                        />
-                        <CardContent >
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                Help combat homelessness in the veteran community
-                            </Typography>
-                            <Typography variant="body2">
-                                <List>
-                                    <ListItem>
-                                        <ListItemText>
-                                            <Typography>
-                                                1. Identify homeless veteran in your local area.
-                                            </Typography>
-                                        </ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText>
-                                            <Typography>
-                                                2. Connect with veteran
-                                            </Typography>
-                                        </ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText>
-                                            <Typography>
-                                                3. Input vetran information into database.
-                                            </Typography>
-                                        </ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText>
-                                            <Typography>
-                                                4. Obtain military records
-                                            </Typography>
-                                        </ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText>
-                                            <Typography>
-                                                5. Enroll veteran in the VA system.
-                                            </Typography>
-                                        </ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText>
-                                            <Typography>
-                                                6. Provide assistance in other areas (health, education, employment)
-                                            </Typography>
-                                        </ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText>
-                                            <Typography>
-                                                7. Close out case
-                                            </Typography>
-                                        </ListItemText>
-                                    </ListItem>  
-                                </List>
-                                <br />
-                                {'**Limited to four (4) sponsorship at a time**'}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small" onClick={beSponsor}>Learn More</Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-            </Grid>
-        </>
-    )
+  const navigate = useNavigate();
+  return (
+    <div className="bg-[url('/src/assets/splash.jpeg')] bg-cover bg-blend-overlay items-center w-full h-screen ">
+      <div className="bg-stone-500 bg-opacity-70 h-full w-full drop-shadow-2xl ">
+        <div className="flex flex-col h-screen justify-center items-center">
+        <img className="mb-2" src={Logo} alt="Vet 4 You" />
+        <h3 className="text-2xl mb-2 text-gray-200 font-bold font-mono drop-shadow-xl">
+          Fight for those who fought for you
+        </h3>
+        <p className="text-gray-200 font-bold font-mono mb-8 drop-shadow-xl">
+          We aim to fight the homeless veterans crisis through the power over community sponsorship
+        </p>
+
+        <button onClick={() => navigate('/login')} className="bg-white font-bold rounded-full py-4 px-8 drop-shadow-xl uppercase tracking-wider hover:bg-stone-700 hover:text-white font-mono">
+          Sponsor A Veteran
+        </button>
+        </div>
+      </div>
+    </div> 
+  );
 }
 
-export default Home
+export default Home;
