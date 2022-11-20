a29f973940ef46b3acf3a368d5d5cca4a7caa5ba
Michael Ocasio <mocasio@sstlift.com>
2022-11-20 12:56:57 -0500

    Addition Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {'Copyright © '}
+        Connect4Vets
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function StickyFooter() {
+  return (
+    <Box
+      sx={{
+        flexDirection: "row",
+        position: "fixed",
+        left: "0",
+        bottom: "0",
+        right: "0",
+
+      }}
+    >
+      <CssBaseline />
+      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
+      </Container>
+      <Box
+        component="footer"
+        sx={{
+          textAlign: "center",
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: "#CCCC99"
+          
+
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            Connect4Vets - It's Time To Bring 'em Home
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
+  );
+}
